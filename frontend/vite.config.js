9b7139a70391766df9757d2a19f47ed8688228c9
Luiz Felipe <felipe2003zuckado@gmail.com>
2026-09-23 23:22:00 -0300

    corrigindo testes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,8 +5,8 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   test: {
-    enviroments: 'jsdom',
+    enviroment: 'jsdom',
     setupFiles: './setupTests.js',
-    global: true
+    globals: true
   }
 })

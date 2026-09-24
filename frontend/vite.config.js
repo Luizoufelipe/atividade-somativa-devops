a300ff1b56418db116eb99110fd3440243c38cb7
Luiz Felipe <felipe2003zuckado@gmail.com>
2026-09-23 22:35:49 -0300

    adicionados 5 testes unitários

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    enviroments: 'jsdom',
+    setupFiles: './setupTests.js',
+    global: true
+  }
 })
